d5350d045efc8173787692a891077386c4f4bce3
Mieszko Bańczerowski <mieszko.banczerowski@gmail.com>
2018-03-10 14:42:04 +0100

    fix no-input-FFT bug; explicitely convert to grayscale when loading imgs; display better

--- code/debluring/deblur.py
+++ code/debluring/deblur.py
@@ -6,39 +6,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 from icecream import ic
 
-sys.path.insert(1, str(pathlib.Path(__file__).parent / '..'))
+root_path = pathlib.Path(__file__).absolute().parents[1]
+sys.path.insert(1, str(root_path))
 from helpers import readImages, Boardspace
 
+# -------------------------------------settings-------------------------------
+show = False
+write = True
 
 # -------------------------------------reading images-------------------------
 
-root_path = pathlib.Path(__file__).parents[1].absolute()
 input_path = root_path / 'sample_data'
 output_path = root_path / 'sample_results'
 
 filenames = [str(name) for name in input_path.iterdir()]
-h_in, u_in = readImages(filenames)
+u_in, h_in = readImages(filenames, color_mode=0)  # 0=grayscale
 
 # -------------------------------------computing FFT--------------------------
 
 kowalsky = 0.001
-H_in, U_in = list(map(lambda x: np.fft.fftshift(x), (h_in, u_in)))
+H_in = np.fft.fftshift(np.fft.fft2(h_in))
+U_in = np.fft.fftshift(np.fft.fft2(u_in))
+H_magnitude = 20*np.log(np.abs(H_in+kowalsky))
 U_magnitude = 20*np.log(np.abs(U_in+kowalsky))
 
-plt.subplot(121);
-plt.imshow(u_in, cmap='gray');
-plt.subplot(122);
-plt.imshow(U_magnitude, cmap='gray')
-plt.savefig(str(output_path / 'u_in and U_in.png'), bbox_inches='tight', dpi=130)
+plt.subplot(221)
+plt.imshow(u_in, cmap='gray')
+plt.title('u')
 
-# -------------------------------------deconvolution by dividing FFTs---------
+plt.subplot(222);
+plt.imshow(h_in, cmap='gray')
+plt.title('PSF')
+
+plt.subplot(223);
+plt.imshow(U_magnitude, cmap='gray')
+plt.title('U magnitude')
+
+plt.subplot(224);
+plt.imshow(H_magnitude, cmap='gray')
+plt.title('OTF magnitude')
+
+if show:
+    plt.show()
+elif write:
+    plt.savefig(str(output_path / 'input_and_FFTs.png'), bbox_inches='tight', dpi=130)
+
+# -------------------------------------dividing FFTs-------------------------
 
 U_deblur = np.divide(U_in, H_in + kowalsky)
+U_deblur_magnitude = 20*np.log(np.abs(U_deblur))
+
+plt.subplot(121);
+plt.imshow(U_deblur_magnitude, cmap='gray')
+plt.title('20*log(abs(U/H + kowalsky))')
+
+# -------------------------------------inverse FFT----------------------------
+
 u_deblur = np.fft.ifft2(np.fft.ifftshift(U_deblur))
 u_deblur_magnitude = 20*np.log(np.abs(u_deblur))
 
-plt.subplot(111);
+plt.subplot(122);
 plt.imshow(u_deblur_magnitude, cmap='gray');
-plt.savefig(str(output_path / 'deblured_broken.png'), bbox_inches='tight', dpi=130)
+plt.title('20*log(abs(iFFT(U/H+kowalsky))')
 
+if show:
+    plt.show()
+elif write:
+    plt.savefig(str(output_path / 'deblured.png'), bbox_inches='tight', dpi=130)
 
