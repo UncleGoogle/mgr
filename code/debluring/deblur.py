dfb34f5600cda638410174bf3159a5ece99e4f13
Mieszko Bańczerowski <mieszko.banczerowski@gmail.com>
2018-03-17 17:31:41 +0100

    better resolution; delete unused files and broken results

--- code/debluring/deblur.py
+++ code/debluring/deblur.py
@@ -15,17 +15,13 @@ root_path = pathlib.Path(__file__).absolute().parents[1]
 parser = argparse.ArgumentParser()
 parser.add_argument('image', type=str, help="Blured image")
 parser.add_argument('psf', type=str, help="PSF of the corresponding image")
-parser.add_argument('--preview', type=bool, help="Do not save results, but show them",
-                    default=True)
+parser.add_argument('--output', type=str, help="where to store results")
 parser.add_argument('--kowalsky', type=float, help="factor to enhance divition",
                     default=0.0001)
-parser.add_argument('--output', type=str, help="output path to store result images",
-                    default=str(root_path / 'results'))
 
 args = parser.parse_args()
-preview = True if args.output else False
-kowalsky = args.kowalsky
 output_path = pathlib.Path(args.output).resolve()
+kowalsky = args.kowalsky
 
 # -------------------------------------reading images-------------------------
 
@@ -59,6 +55,21 @@ U_in = np.fft.fftshift(np.fft.fft2(u_in))
 H_magnitude = 20*np.log(np.abs(H_in+kowalsky))
 U_magnitude = 20*np.log(np.abs(U_in+kowalsky))
 
+# -------------------------------------dividing FFTs-------------------------
+U_deblur = np.divide(U_in, H_in + kowalsky)
+U_deblur_show = 20*np.log(np.abs(U_deblur))
+# phase = np.arctan2(U_deblur.imag, U_deblur.real)
+
+# -------------------------------------inverse FFT----------------------------
+
+u_deblur = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(U_deblur)))
+u_deblur_show = 2*np.abs(u_deblur)
+# u_deblur_show = u_deblur_show / max(np.ndarray.flatten(u_deblur_show))
+
+# -------------------------------------DRAWING RESULTS------------------------
+DPI=96
+plt.figure(figsize=(1200/DPI, 800/DPI), dpi=DPI)
+
 plt.subplot(231)
 plt.imshow(u_in, cmap='gray')
 plt.title('u')
@@ -75,26 +86,15 @@ plt.subplot(235);
 plt.imshow(H_magnitude, cmap='gray')
 plt.title('OTF')
 
-# -------------------------------------dividing FFTs-------------------------
-U_deblur = np.divide(U_in, H_in + kowalsky)
-U_deblur_show = 20*np.log(np.abs(U_deblur))
-# phase = np.arctan2(U_deblur.imag, U_deblur.real)
-
 plt.subplot(236);
 plt.imshow(U_deblur_show, cmap='gray')
 plt.title('U/OTF')
-
-# -------------------------------------inverse FFT----------------------------
-
-u_deblur = np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(U_deblur)))
-u_deblur_show = 2*np.abs(u_deblur)
-# u_deblur_show = u_deblur_show / max(np.ndarray.flatten(u_deblur_show))
 
 plt.subplot(233);
 plt.imshow(u_deblur_show, cmap='gray');
 plt.title('u_deblured = iFT(U/OTF)')
 
-if args.preview:
-    plt.show()
+if output_path:
+    plt.savefig(output_path, bbox_inches='tight', dpi=DPI*2)
 else:
-    plt.savefig(output_path, bbox_inches='tight', dpi=130)
+    plt.show()
